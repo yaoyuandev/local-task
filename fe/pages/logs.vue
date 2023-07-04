74254bb769d905464b7d67d23d5d1395d77bd215
Yao Yuan <yaoyuanyy@outlook.com>
2023-07-04 23:40:41 +0800

    remove poll log

--- fe/pages/logs.vue
+++ fe/pages/logs.vue
@@ -5,25 +5,9 @@
 </template>
 <script>
 export default {
-  data() {
-    return {
-      log: "",
-    };
-  },
-  mounted() {
-    this.startInterval();
-  },
-  methods: {
-    async getLogs() {
-      this.log = (
-        await this.$axios.get(`/api/logs?file=${this.$route.query.file}`)
-      ).data;
-    },
-    startInterval() {
-      setInterval(async () => {
-        await this.getLogs();
-      }, 1000);
-    },
+  async asyncData({ query, $axios }) {
+    const log = await $axios.$get(`/api/logs?file=${query.file}`);
+    return { log };
   },
 };
 </script>
